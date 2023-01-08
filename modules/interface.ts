c976e646cfd3404e06cebd8787cff801005e0a9d
Nguyễn Hữu Phúc <phuc82908@gmail.com>
2023-01-08 13:54:39 +0700

    interface in typescript

---
+++ modules/interface.ts
@@ -0,0 +1,12 @@
+/* Interfaces in typescript allow you to define what properties and methods are needed by the object to be implemented.
+ If the object conforms to the correct interface template, then the object that implements that interface will be properly 
+ implemented. If the interface is not implemented properly then the typescript will throw an error immediately. */
+
+interface Person {
+  name: string;
+  age: number;
+  hobbies: string[];
+  position: string;
+}
+
+const simplePerson: Person = { name: "sam", age: 23, hobbies: ["soccer", "piano"], position: "dev" }
